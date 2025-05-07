1593b6c7c7c5f9f3dcbcf370773002f340b50f02
Garongan <alvindo56@gmail.com>
2025-05-07 20:23:07 +0700

    feat: navigation bar

---
+++ Ivan's story/IvanStoryDetail.swift
@@ -0,0 +1,18 @@
+//
+//  IvanStoryDetail.swift
+//  Ivan's story
+//
+//  Created by Alvindo Tri Jatmiko on 07/05/25.
+//
+
+import SwiftUI
+
+struct IvanStoryDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IvanStoryDetail()
+}
