840a18a88baaad815b288c71c03b025beff1cb06
Garongan <alvindo56@gmail.com>
2025-05-07 20:23:40 +0700

    feat: first screen layout

--- Ivan's story/ContentView.swift
+++ Ivan's story/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 16) {
+                Image("ProfilePic")
+                    .resizable()
+                    .frame(height: 400)
+                    .cornerRadius(8)
+                
+                Text("Hello, I am Ivan")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                NavigationLink("See my story", destination: IvanStoryDetail())
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.accentColor)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(8)
+                                
+            }
+            .padding(16)
+            .navigationTitle("Ivan's Story")
         }
-        .padding()
     }
 }
 
