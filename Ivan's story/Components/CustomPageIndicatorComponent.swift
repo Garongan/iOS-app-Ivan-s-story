0d80f2d45cc50c754e95f3fc3b3fe9568f0eb117
Garongan <alvindo56@gmail.com>
2025-05-09 17:54:49 +0700

    feat: story view

---
+++ Ivan's story/Components/CustomPageIndicatorComponent.swift
@@ -0,0 +1,18 @@
+//
+//  CustomPageIndicatorComponent.swift
+//  Ivan's story
+//
+//  Created by Alvindo Tri Jatmiko on 09/05/25.
+//
+
+import SwiftUI
+
+struct CustomPageIndicatorComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomPageIndicatorComponent()
+}
---
+++ Ivan's story/Components/StoryComponent.swift
@@ -0,0 +1,18 @@
+//
+//  StoryComponent.swift
+//  Ivan's story
+//
+//  Created by Alvindo Tri Jatmiko on 09/05/25.
+//
+
+import SwiftUI
+
+struct StoryComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryComponent()
+}
